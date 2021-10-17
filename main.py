4ef9de842f73b2006ab47249c1ca0aa37dbc5c9f
Jakub Siejak <52972584+Kinoruu@users.noreply.github.com>
2021-10-17 23:27:29 +0200

    better for all samples

--- main.py
+++ main.py
@@ -2,54 +2,64 @@ import argparse
 import cv2
 import numpy as np
 
-img = cv2.imread('00000010_000.png', cv2.IMREAD_COLOR)
-img2 = cv2.imread('00000010_000.png', cv2.IMREAD_COLOR)
-img3 = cv2.imread('00000010_000.png', cv2.IMREAD_COLOR)
-h, w, c= img.shape
-#cv2.imshow('a', img)
+img = cv2.imread('00000011_000.png', cv2.IMREAD_COLOR)
+img2 = cv2.imread('00000011_000.png', cv2.IMREAD_COLOR)
+img3 = cv2.imread('00000011_000.png', cv2.IMREAD_COLOR)
+h, w, c = img.shape
+cv2.imshow('normal', img)
 
-img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-img_gb = cv2.GaussianBlur(img,(15,15),cv2.BORDER_DEFAULT)
-#cv2.imshow('b', img_gb)
-ret, thresh1 = cv2.threshold(img_gb, 100, 255, cv2.THRESH_BINARY)
-#cv2.imshow('c', thresh1)
+mean = np.mean(img)
+mean -= 24
+
+img = cv2.GaussianBlur(img,(3,3),cv2.BORDER_DEFAULT)
+cv2.imshow('blur', img)
+
+for iter1 in range(h):
+    for iter2 in range(w):
+        val = img[iter1,iter2,1]
+        if (val < mean):
+            img[iter1,iter2] = [0,0,0]
+
+cv2.imshow('dark', img)
+
+img = cv2.GaussianBlur(img,(3,3),cv2.BORDER_DEFAULT)
+cv2.imshow('blur', img)
+
+
+ret, thresh1 = cv2.threshold(img, mean, 255, cv2.THRESH_BINARY)
+cv2.imshow('threshold', thresh1)
 
 contours, hierarchy = cv2.findContours(thresh1[:,:,0], cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-#77,22,174
+# 77,22,174
 img_cont = cv2.drawContours(img, contours, -1,(60,250,74),2)
-#cv2.imshow('d', img_cont)
+cv2.imshow('all_contours', img_cont)
 
 contourss = []
 contourss2 = []
 contourss3 = []
 
 for c in contours:
-    #epsilon = 0.001 * cv2.arcLength(c, True)
-    #approx = cv2.approxPolyDP(c, epsilon, True)
     hull = cv2.convexHull(c)
     contourss.append(hull)
 
-for c in contourss:
-    if (cv2.contourArea(c) > 10000) and (cv2.contourArea(c) < (0.5 * h * w)):
-        contourss2.append(c)
+img_cont_1 = cv2.drawContours(img2, contourss, -1,(200,0,200), 2)
+cv2.imshow('all_contours_hulled', img_cont_1)
 
-print(len(contours))
-print(len(contourss))
-print(len(contourss2))
+for c in contourss:
+    if (cv2.contourArea(c) > (0.04 * h * w)) and (cv2.contourArea(c) < (0.5 * h * w)):
+        contourss2.append(c)
 
 for c in contourss2:
     temp = np.zeros_like(img3)
     cv2.drawContours(temp, [c], 0, (255, 255, 255), -1)
-    if np.mean(img3[temp == 255]) < 120:
+    if np.mean(img3[temp == 255]) < mean:
         contourss3.append(c)
-
-print(len(contourss3))
 
 contourss4 = []
 
-#hull = cv2.convexHull(cnt)
+# hull = cv2.convexHull(cnt)
 
-img_cont_2 = cv2.drawContours(img2, contourss3, -1,(0,0,200), cv2.FILLED)
+img_cont_2 = cv2.drawContours(img3, contourss3, -1,(0,0,200), cv2.FILLED)
 cv2.imshow('e', img_cont_2)
 
 cv2.waitKey(0)
